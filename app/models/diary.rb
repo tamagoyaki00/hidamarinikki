c1dc43ff6d07bd27886cedebd15df57cf4a55eca
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-02 21:38:34 +0900

    feat:UserモデルとDiaryモデルのアソシエーションとバリデーションを定義

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,8 @@
 class Diary < ApplicationRecord
+  belongs_to :user
+
+  validates :body, presence: true, length: { maximum: 1000}
+  validates :posted_date, presence: true
+
+  enum status: { private: 0, public: 1 }
 end
