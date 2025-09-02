c2473bfb158c1af9fb23958185f7211e02896fc6
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-02 10:32:37 +0900

    fix:エラー発生の為バリデーションを一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :uid, uniqueness: { scope: :provider }, if: :provider?
 
 
   def self.from_omniauth(auth)
