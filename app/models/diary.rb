4764c2ebdfa3e3ca3a82ee7381c9966a24763d5c
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-04 16:46:20 +0900

    fix:enumをキーワード引数から位置引数に修正

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -5,5 +5,5 @@ class Diary < ApplicationRecord
   validates :happiness_count, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 0 }
   validates :posted_date, presence: true
 
-  enum status: { is_public: 0, is_private: 1 }
+  enum :status, { is_public: 0, is_private: 1 }
 end
