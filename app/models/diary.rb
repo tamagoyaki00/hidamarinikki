1867c87da354b783aac36b3f341ee3c734b76b30
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-04 12:23:48 +0900

    feat:バリデーションとアソシエーションを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,7 +1,8 @@
 class Diary < ApplicationRecord
   belongs_to :user
-
-  validates :body, presence: true, length: { maximum: 1000}
+  has_many :diary_contents, dependent: :destroy
+  
+  validates :happiness_count, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 0 }
   validates :posted_date, presence: true
 
   enum status: { is_public: 0, is_private: 1 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :diaries
+  has_many :diaries, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
