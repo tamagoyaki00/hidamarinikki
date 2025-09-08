6c19603503043d6c534945a6b3d1f2b52cff4201
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-08 17:09:36 +0900

    feat:diary、tag、diary_tagモデルにバリデーションとアソシエーションを定義

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,6 @@
 class Tag < ApplicationRecord
+  has_many :diary_tags, dependent: :destroy
+  has_many :diaries, through: :diary_tags
+
+  validates :name, presence: true, length: { maximum: 20 }
 end
