859ebc56330db45d490753d46d90e1f98b846c1a
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-14 16:03:26 +0900

    feat:アバターのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,28 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 20 }
   validates :uid, uniqueness: { scope: :provider }, if: :provider?
-
+  validate :validate_avatar_format
+  validate :avatar_size
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  private
+
+  def validate_avatar_format
+    if avatar.attached? && !avatar.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, "：ファイル形式が、JPEG, PNG, GIF以外になってます。ファイル形式をご確認ください")
+    end
+  end
+
+  def avatar_size
+    if avatar.attached? && avatar.byte_size > 5.megabytes
+      errors.add(:avatar, "のファイルサイズは5MB以内にしてください")
     end
   end
 end
