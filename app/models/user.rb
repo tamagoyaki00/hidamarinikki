fedae5e3359543b1a0d4b9afbe2c3f5b572705ae
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-14 20:25:20 +0900

    fix:プロ委フィール編集画面のuiを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :diary_contents, through: :diaries, dependent: :destroy
 
   has_one_attached :avatar do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :thumb, resize_to_limit: [200, 200]
   end
 
   # Include default devise modules. Others available are:
