df6d5cdc58cb3fc725eeab8825ac3a869c2a925b
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-20 12:08:47 +0900

    feat:DiaryReminderJobを一分毎に実行するように記述

---
+++ config/schedule.rb
@@ -0,0 +1,16 @@
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+# cronを実行する環境変数周りの設定
+ENV.each { |k, v| env(k, v) }
+rails_env = ENV["RAILS_ENV"] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+
+#ログの表示場所
+set :output, "log/cron.log"
+
+
+#一分毎に以下のファイルを実行
+every 1.minutes do
+  runner "DiaryReminderJob.perform_now"
+end
