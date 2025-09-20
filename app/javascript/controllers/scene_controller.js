7e86dd0dc3ec341828ed4c905e699f741077d3ac
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-20 09:36:57 +0900

    fix:scene_nameに値を設定しても、パラメータに値が入っていなかった問題を修正

--- app/javascript/controllers/scene_controller.js
+++ app/javascript/controllers/scene_controller.js
@@ -1,18 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["preset", "custom"]
+  static targets = ["preset", "custom", "presetSelect", "customText"]
 
   connect() {
-    this.toggle() // 初期状態を反映
+    this.toggle()
   }
 
   toggle() {
-    const selected = this.element.querySelector(
-      "input[name='notification_setting[scene_type]']:checked"
-    )?.value
+    const isPreset = document.getElementById("scene_preset").checked
 
-    this.presetTarget.classList.toggle("hidden", selected !== "preset")
-    this.customTarget.classList.toggle("hidden", selected !== "custom")
+    // プリセット用フィールドの表示/非表示を切り替える
+    this.presetTarget.classList.toggle("hidden", !isPreset)
+    // プリセットのselectを有効化/無効化
+    this.presetSelectTarget.disabled = !isPreset
+
+    // カスタム用フィールドの表示/非表示を切り替える
+    this.customTarget.classList.toggle("hidden", isPreset)
+    // カスタムのtext_fieldを有効化/無効化
+    this.customTextTarget.disabled = isPreset
   }
 }
