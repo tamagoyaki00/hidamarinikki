95934c6b4d4f4dce2c6f8797e2819af21b65a5c4
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-22 10:33:38 +0900

    feat:設定した時間にプッシュ通知を届ける実装

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,21 @@
 # Rails.rootを使用するために必要
 require File.expand_path(File.dirname(__FILE__) + "/environment")
+
 # cronを実行する環境変数周りの設定
 ENV.each { |k, v| env(k, v) }
 rails_env = ENV["RAILS_ENV"] || :development
-# cronを実行する環境変数をセット
 set :environment, rails_env
 
-#ログの表示場所
-set :output, "log/cron.log"
+# ログ出力
+set :output, "/myapp/log/cron.log"
 
+# ジョブ実行にbashを使う
+set :job_template, "/bin/bash -l -c ':job'"
 
-#一分毎に以下のファイルを実行
+# bundle exec rails runner をフルパスで指定
+job_type :runner, "cd :path && /usr/local/bundle/bin/bundle exec rails runner -e :environment ':task' :output"
+
+# 1分ごとにジョブ実行
 every 1.minutes do
-  runner "DiaryReminderJob.perform_later"
+  rake "diary_reminder:remind"
 end
