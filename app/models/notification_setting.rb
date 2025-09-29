188b00c1e268c76bd4da61c6b14cfc4586fc75d2
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-29 22:41:07 +0900

    feat:設定時間の15分単位のバリデーションを追加

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -4,4 +4,13 @@ class NotificationSetting < ApplicationRecord
   validates :scene_type, presence: true
   validates :scene_name, presence: true, if: -> { scene_type == "custom" }
   enum :scene_type, { preset: 0, custom: 1 }
+
+  validate :notification_time_must_be_15_min_interval
+
+  def notification_time_must_be_15_min_interval
+    return unless notification_time
+    unless [0, 15, 30, 45].include?(notification_time.min)
+      errors.add(:notification_time, "は15分単位で設定してください")
+    end
+  end
 end
