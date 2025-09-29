c276f0ef7b83639e81a6e30c5b9388eee6c9ef25
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-29 21:13:21 +0900

    fix:diary_reminder_jobのユーザー取得SQL文に通知時間が現在の時間と一致するかどうかという条件を追加

--- app/jobs/diary_reminder_job.rb
+++ app/jobs/diary_reminder_job.rb
@@ -8,36 +8,20 @@ class DiaryReminderJob < ApplicationJob
   def perform
     Rails.logger.info "DiaryReminderJob started at #{Time.current.in_time_zone('Asia/Tokyo').strftime('%Y-%m-%d %H:%M:%S %z')}"
 
-    # 通知時間が設定されていてリマインダー有効なユーザーを取得
-    users = User.joins(:notification_setting)
-                .where(notification_settings: { reminder_enabled: true })
-                .where.not(notification_settings: { notification_time: nil })
-
     current_time_in_jst = Time.current.in_time_zone("Asia/Tokyo")
+    current_hour_minute = current_time_in_jst.strftime('%H:%M')
 
-    users.each do |user|
-      user_setting = user.notification_setting
-      next unless user_setting && user_setting.notification_time
+    # 通知時間が一致するユーザーだけを取得
+    users_to_notify = User.joins(:notification_setting)
+                      .where(notification_settings: { reminder_enabled: true })
+                      .where("to_char(notification_settings.notification_time, 'HH24:MI') = ?", current_hour_minute)
 
-      # 今日のJSTに合わせて通知時間を設定
-      user_notification_time_today_jst = current_time_in_jst.change(
-        hour: user_setting.notification_time.hour,
-        min: user_setting.notification_time.min,
-        sec: 0
-      )
-
-      # 現在時刻の「時」と「分」が通知時間と一致するかチェック
-      if current_time_in_jst.hour == user_notification_time_today_jst.hour &&
-         current_time_in_jst.min == user_notification_time_today_jst.min
-
-        if user.onesignal_external_id.present?
-          Rails.logger.info "Attempting to send notification to user ID: #{user.id} (External ID: #{user.onesignal_external_id}) for scheduled time: #{user_setting.notification_time.strftime('%H:%M')}"
-          send_one_signal_notification(user)
-        else
-          Rails.logger.warn "User ID: #{user.id} has no OneSignal External ID. Skipping notification."
-        end
+    users_to_notify.each do |user|
+      if user.onesignal_external_id.present?
+        Rails.logger.info "Attempting to send notification to user ID: #{user.id} (External ID: #{user.onesignal_external_id}) for scheduled time: #{user.notification_setting.notification_time.strftime('%H:%M')}"
+        send_one_signal_notification(user)
       else
-        Rails.logger.info "User ID: #{user.id}'s notification time #{user_setting.notification_time.strftime('%H:%M')} is not the current minute. (Current: #{current_time_in_jst.strftime('%H:%M')})"
+        Rails.logger.warn "User ID: #{user.id} has no OneSignal External ID. Skipping notification."
       end
     end
 
