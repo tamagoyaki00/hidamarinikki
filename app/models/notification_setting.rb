fd7bc9a095bd1d668ada6b082565e4ccb464ee8c
MayuFujimoto <mayufujimoto41@gmail.com>
2025-09-30 10:31:19 +0900

    feat:通知がONのとき通知時間の設定を必須にするバリデーションを追加

--- app/models/notification_setting.rb
+++ app/models/notification_setting.rb
@@ -1,6 +1,7 @@
 class NotificationSetting < ApplicationRecord
   belongs_to :user
 
+  validates :notification_time, presence: true, if: :reminder_enabled?
   validates :scene_type, presence: true
   validates :scene_name, presence: true, if: -> { scene_type == "custom" }
   enum :scene_type, { preset: 0, custom: 1 }
