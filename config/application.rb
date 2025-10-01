6f933da5b6ed3c997335c479209f922dfcb2d233
MayuFujimoto <mayufujimoto41@gmail.com>
2025-10-01 14:39:20 +0900

    fix:DBのタイムゾーンをJSTにできなかった為、UTCで統一

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Myapp
     # in config/environments, which are processed later.
     #
     config.time_zone = "Asia/Tokyo"
-    config.active_record.default_timezone = :local
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
