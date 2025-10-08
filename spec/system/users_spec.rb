d18079c427db5cb7246e25a6679edae72ab09203
MayuFujimoto <mayufujimoto41@gmail.com>
2025-10-08 19:48:29 +0900

    feat: ログイン・ログアウトに関するシステムスペックテストを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   describe '新規登録' do
-    context '有効な入力した場合'
+    context '有効な入力した場合' do
       it '新規登録が成功すること' do
         visit new_user_registration_path
         fill_in 'ユーザー名', with: 'テストユーザー'
@@ -14,8 +14,9 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_content 'アカウント登録が完了しました'
         expect(User.count).to eq(1)
       end
+    end
 
-    context '必須項目が未入力の場合'
+    context '必須項目が未入力の場合' do
       it 'エラーになり、新規登録が失敗すること' do
         visit new_user_registration_path
         fill_in 'ユーザー名', with: ''
@@ -27,5 +28,63 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_content 'メールアドレスを入力してください'
         expect(page).to have_content 'パスワードを入力してください'
       end
+    end
+  end
+
+  describe 'ログインに関すること' do
+    let!(:user) { create(:user) }
+
+    context '未ログインのとき' do
+      it '認証必須ページにアクセスできない' do
+        visit home_path
+        expect(page).to have_current_path new_user_session_path
+        expect(page).to have_content 'ログインもしくはアカウント登録してください'
+      end
+    end
+
+    context 'ログイン時' do
+      it '正しい情報でログインできる' do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログイン'
+        expect(page).to have_content 'ログインしました'
+        expect(page).to have_current_path authenticated_root_path
+      end
+
+      it '誤った情報ではログインできない' do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: 'wrong@example.com'
+        fill_in 'パスワード', with: 'wrongpass'
+        click_button 'ログイン'
+        expect(page).to have_content 'メールアドレスまたはパスワードが違います'
+      end
+    end
+
+    context 'ログイン後' do
+      before { login_as(user) }
+
+      it 'ブラウザを閉じてもログイン状態が維持される' do
+        Capybara.reset_sessions! # ブラウザを閉じた想定
+        visit authenticated_root_path
+        expect(page).to have_current_path authenticated_root_path
+      end
+
+      it 'ログアウトができること' do
+        click_button '設定'
+        click_link 'ログアウト'
+        expect(page).to have_content 'ログアウトしました'
+      end
+    end
+
+    context 'ログアウト後' do
+      it 'ログアウト後は認証必須ページにアクセスできない' do
+        login_as(user)
+        click_button '設定'
+        click_link 'ログアウト'
+        visit authenticated_root_path
+        expect(page).to have_current_path unauthenticated_root_path
+      end
+    end
   end
 end
