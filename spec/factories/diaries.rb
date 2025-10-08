99b0363c1be1ab4b9d6cdab891fe64a5fe67bd90
MayuFujimoto <mayufujimoto41@gmail.com>
2025-10-08 15:13:09 +0900

    feat: diaryのfactorybotデフォルトのステータスを明示

--- spec/factories/diaries.rb
+++ spec/factories/diaries.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     association :user
     posted_date { Date.current }
     happiness_count { 1 }
+    status { :is_public }
 
     trait :private do
       status { :is_private }
