b352aff27c919e65e10736d80b0c52f1fa570c42
MayuFujimoto <mayufujimoto41@gmail.com>
2025-10-15 19:17:25 +0900

    feat: 日記投稿後と更新後のフラッシュメッセージを変更したのでテストも修正

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -78,7 +78,7 @@ RSpec.describe 'Diaries', type: :system do
           fill_in 'item_3', with: '楽しかったこと3'
           fill_in 'diary_form[tag_names]', with: '日常, 幸せ'
           click_button '投稿する'
-          expect(page).to have_content '日記を投稿しました'
+          expect(page).to have_content '日記投稿ありがとう！'
           expect(page).to have_current_path home_path
         end
       end
@@ -210,7 +210,7 @@ RSpec.describe 'Diaries', type: :system do
         fill_in 'item_1', with: '更新後の内容'
         click_button '更新'
 
-        expect(page).to have_content '日記を更新しました'
+        expect(page).to have_content '日記更新ありがとう！'
         expect(page).to have_current_path home_path
         click_link 'マイ日記'
         expect(page).to have_content '更新後の内容'
