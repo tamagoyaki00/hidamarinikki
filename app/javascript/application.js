7105e5ead409d154b6457a5df30cb58bd2551b64
MayuFujimoto <mayufujimoto41@gmail.com>
2025-11-03 23:10:12 +0900

    feat: swiperの初期化を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,20 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
+document.addEventListener("turbo:load", () => {
+  new Swiper('.swiper', {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+})
+
