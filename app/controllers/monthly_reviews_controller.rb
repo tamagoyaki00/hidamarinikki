9b475f5010eb4f656267a1f55f341253f8e70e2d
MayuFujimoto <mayufujimoto41@gmail.com>
2025-11-07 19:14:13 +0900

    fix: DBに保存するfind_or_create_byを削除

--- app/controllers/monthly_reviews_controller.rb
+++ app/controllers/monthly_reviews_controller.rb
@@ -14,19 +14,19 @@ class MonthlyReviewsController < ApplicationController
       return render :show
     end
 
-    @monthly_review = current_user.monthly_reviews.find_or_create_by(month: month) do |review|
-      total = diaries.sum(:happiness_count)
-      average = diaries.any? ? (total.to_f / diaries.count).round(2) : 0
-      max_diary = diaries.max_by(&:happiness_count)
+    total = diaries.sum(:happiness_count)
+    average = diaries.any? ? (total.to_f / diaries.count).round(2) : 0
+    max_diary = diaries.max_by(&:happiness_count)
 
-      review.total_happiness_count = total
-      review.average_happiness_count = average
-      review.max_happiness_count = max_diary&.happiness_count
-      review.max_happiness_diary = max_diary
-      review.diary_snippets = diaries.flat_map do |d|
+    @monthly_review = {
+      total_happiness_count: total,
+      average_happiness_count: average,
+      max_happiness_count: max_diary&.happiness_count,
+      max_happiness_diary: max_diary,
+      diary_snippets: diaries.flat_map do |d|
         d.diary_contents.map { |c| { date: d.posted_date, body: c.body } }
       end.sample(3)
-    end
+    }
 
     render :show
   end
