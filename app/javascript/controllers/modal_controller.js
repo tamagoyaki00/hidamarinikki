5b33e5a40f0fe72a86f157abb9ec3601befa911f
MayuFujimoto <mayufujimoto41@gmail.com>
2025-11-12 15:06:45 +0900

    fix: モーダルからページ遷移後に戻るボタンを押したら再度モーダルが表示される問題を修正

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,6 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
+  connect() {
+    document.addEventListener("turbo:before-visit", this.closeIfOpen)
+  }
+
+  disconnect() {
+    document.removeEventListener("turbo:before-visit", this.closeIfOpen)
+  }
+
   open() {
     this.modalTarget.showModal()
   }
@@ -13,6 +21,12 @@ export default class extends Controller {
 
   closeOnBackdrop(event) {
     if (event.target === this.modalTarget) {
+      this.close()
+    }
+  }
+
+  closeIfOpen = () => {
+    if (this.hasModalTarget && this.modalTarget.open) {
       this.close()
     }
   }
