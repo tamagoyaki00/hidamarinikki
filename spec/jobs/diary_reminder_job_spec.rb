9065eeb39e950e7a38d0fa495cacbc611aa8d0d7
MayuFujimoto <mayufujimoto41@gmail.com>
2025-11-20 23:22:37 +0900

    feat: diary_reminder_jobのテストを追加

---
+++ spec/jobs/diary_reminder_job_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe DiaryReminderJob, type: :job do
+let!(:user_on_time) do
+  create(:user).tap do |u|
+    u.notification_setting.update!(
+      reminder_enabled: true,
+      notification_time: Time.zone.parse("09:00"),
+      scene_type: :preset,
+      scene_name: "朝のスタート"
+    )
+  end
+end
+
+  let!(:user_off) {create(:user)}
+
+  it "通知ONかつ時間一致ユーザーのみAPI通信される" do
+    job = DiaryReminderJob.new
+    allow(job).to receive(:send_one_signal_notification)
+
+    travel_to Time.zone.parse("09:00") do
+      job.perform
+    end
+
+    expect(job).to have_received(:send_one_signal_notification).with(user_on_time)
+  end
+end
